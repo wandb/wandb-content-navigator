bd09bc2c26c1eb1307f45f93e7dd0d03f11c1102
Morgan McGuire <morganmcguire@Morgans-MacBook-Pro.local>
2024-01-22 20:16:57 +0000

    fix

--- slack_app.py
+++ slack_app.py
@@ -66,8 +66,10 @@ async def handle_app_mentions(event, say, logger):
     logger.info(f"RESPONSE: LEN: {len(cleaned_response)}, {cleaned_response}")
 
     ### SEND CONTENT SUUGESTIONS BACK TO SLACK USER ###
+    # Response if no content suggestions found
     if len(cleaned_response) == 0:
         slack_text = f"Hey <@{user}>, no content suggestions found. Try rephrasing your query."
+    # Response if content suggestions found
     else:
         slack_text = f"Hey <@{user}>, content suggestions below:\n\n"
         for explanation, source, score in cleaned_response[:N_SOURCES_TO_SEND]:
@@ -79,8 +81,9 @@ async def handle_app_mentions(event, say, logger):
             if '--debug' not in user_query:
                 slack_text += f"• {explanation.content_description} - <{unquote(source)}|Link> \n\n" #*{source}*\n\n" # *<{source}|Link>*\n\n"
             else:
-                slack_text += f"score: {score}, Source: {source}\nreason_why_helpful: {explanation.reason_why_helpful}\n\
-    content_is_relevant: {explanation.content_is_relevant}\nDescription: {explanation.content_description}\n\n"
+                slack_text += f"*Score*: {score}, *Source*: {source}\n*reason_why_helpful*:\
+{explanation.reason_why_helpful}\n*chain_of_thought*: {explanation.chain_of_thought}\n\
+*content_is_relevant*: {explanation.content_is_relevant}\n*content_description*: {explanation.content_description}\n\n"
     
     await say(slack_text, channel=SLACK_CHANNEL_ID, thread_ts=ts)
     logger.info(f"Sent message: {slack_text}")
@@ -89,8 +92,9 @@ async def handle_app_mentions(event, say, logger):
     if '--debug' in user_query:
         rejected_responses = [(explanation, source, score) for explanation, source, score in response if explanation.content_is_relevant is not True]
         for explanation, source, score in rejected_responses:
-            slack_response = f" REJECTED, score: {score}, Source: {source}\nreason_why_helpful: {explanation.reason_why_helpful}\n\
-content_is_relevant: {explanation.content_is_relevant}\nDescription: {explanation.content_description}\n\n"
+            slack_response = f"REJECTED, *Score*: {score}, *Source*: {source}\n*reason_why_helpful*:\
+{explanation.reason_why_helpful}\n*chain_of_thought*: {explanation.chain_of_thought}\n\
+*content_is_relevant*: {explanation.content_is_relevant}\n*content_description*: {explanation.content_description}\n\n"
             await say(slack_response, channel=SLACK_CHANNEL_ID, thread_ts=ts)
         # logger.info(f"Sent message: {slack_response}")
 
