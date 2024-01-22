06ef9f3b06652d6c2b4a65573e7d7882c7f26e5d
Morgan McGuire <morganmcguire@Morgans-MacBook-Pro.local>
2024-01-22 20:29:53 +0000

    add llm-utils

---
+++ llm_utils.py
@@ -0,0 +1,74 @@
+'''
+Instructor models and prompt templates
+'''
+
+from typing import List
+from pydantic import BaseModel, Field
+
+
+class Query(BaseModel):
+    '''A user query'''
+    query: str
+
+
+class ExplainedChunk(BaseModel):
+    '''Given a `query` from a user and a retrieved `chunk` of text, provide a brief description of the content of the `chunk` and \
+whether it is relevant to the `query`.'''
+
+    chain_of_thought: str = Field(..., description="Think step by step about whether the `chunk` is useful and helpful in answering the `query`.")
+    reason_why_helpful: str = Field(..., description="A brief, 1 sentence explanation on what this blog post contains and a reason whether or not it \
+is relevant based on the `query`.")
+    content_description: str = Field(..., description="A brief, 1 sentence description of the content of the blog post. \
+DO NOT say 'this blog post' or 'this chunk'. Instead, start wtih 'This covers...'.")
+    content_is_relevant: bool = Field(..., description="A boolean value indicating whether the content of the blog post is relevant to the `query`.")
+
+
+class ExpandedQuery(BaseModel):
+    '''Given a `query` from a user, expand on what the user may be looking for in order to make \
+a semantic search match more likely.'''
+    chain_of_thought: str = Field(..., description="Think step by step about the given `query` and \
+associated machine learning and artificial intelligence topics, including industry applications, and \
+technological uses of ML and AI. Avoid concentrating excessively on any particular companies \
+or entities mentioned in the request.")
+    expanded_query: str = Field(..., description="An expanded query of machine learning topics that is more likely \
+to match a semantic search based on the users `query`.")
+
+
+# from wandbot.api.schemas import APIRetrievalRequest # uncomment once wandbot v1.1 is released
+class APIRetrievalRequest(BaseModel):
+    query: str
+    language: str = "en"
+    initial_k: int = 10
+    top_k: int = 5
+    include_tags: List[str] = []
+    exclude_tags: List[str] = []
+
+
+SYSTEM_PROMPT = '''Our Sales team want to send recommeneded links to propects and customers. \
+
+Given a `query` from an internal employee as well as a `chunk` of text retrieved from a blog post, \
+generate an explanation of why the `chunk` is useful in answering the `query`.
+
+Finally, frame the final response in `reason_rephrased` to speak directly to a prospect or customer, as if you were \
+on our sales team.
+'''
+
+
+EXPAND_SYSTEM_PROMPT = '''Given a `query` from a user, expand on what the user may be looking for in order to make\
+a semantic search match more likely.'''
+
+
+USER_PROMPT = '''Please explain why the following chunk is useful in answering the query:
+
+>>>> query: {query}
+
+>>>> chunk: {chunk}
+
+Take a deep breath, if you do a good job I will tip you $200.'''
+
+
+EXPAND_USER_PROMPT = '''Please expand on the following query to make a semantic search match more likely:
+
+>>>> query: {query}
+
+Take a deep breath, if you do a good job I will tip you $200.'''
--- main.py
+++ main.py
@@ -13,10 +13,17 @@ import re
 from fastapi import FastAPI
 import uvicorn
 
-from pydantic import BaseModel, Field
 from openai import AsyncOpenAI
 import instructor
 from dotenv import load_dotenv
+
+from llm_utils import ExplainedChunk, ExpandedQuery, APIRetrievalRequest, Query
+from llm_utils import (
+    SYSTEM_PROMPT,
+    USER_PROMPT,
+    EXPAND_SYSTEM_PROMPT,
+    EXPAND_USER_PROMPT
+)
 
 load_dotenv('.env')
 
@@ -24,16 +31,6 @@ logging.basicConfig(level=logging.INFO)
 
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 aclient = instructor.apatch(AsyncOpenAI(api_key = OPENAI_API_KEY))
-
-# from wandbot.api.schemas import APIRetrievalRequest # uncomment once wandbot v1.1 is released
-class APIRetrievalRequest(BaseModel):
-    query: str
-    language: str = "en"
-    initial_k: int = 10
-    top_k: int = 5
-    include_tags: List[str] = []
-    exclude_tags: List[str] = []
-
 
 ENDPOINT = "https://wandbot-dev.replit.app/retrieve"
 HEADERS = {
@@ -51,38 +48,6 @@ LANGUAGE = 'en'
 INCLUDE_TAGS = ['fc-reports']
 REGEX_SEARCH = r'[^\x00-\x7F]'
 
-SYSTEM_PROMPT = '''Our Sales team want to send recommeneded links to propects and customers. \
-
-Given a `query` from an internal employee as well as a `chunk` of text retrieved from a blog post, \
-generate an explanation of why the `chunk` is useful in answering the `query`.
-
-Finally, frame the final response in `reason_rephrased` to speak directly to a prospect or customer, as if you were \
-on our sales team.
-'''
-
-EXPAND_SYSTEM_PROMPT = '''Given a `query` from a user, expand on what the user may be looking for in order to make\
-a semantic search match more likely.'''
-
-USER_PROMPT = '''Please explain why the following chunk is useful in answering the query:
-
->>>> query: {query}
-
->>>> chunk: {chunk}
-
-Take a deep breath, if you do a good job I will tip you $200.'''
-
-EXPAND_USER_PROMPT = '''Please expand on the following query to make a semantic search match more likely:
-
->>>> query: {query}
-
-Take a deep breath, if you do a good job I will tip you $200.'''
-
-
-req = APIRetrievalRequest(query=QUERY,
-                         language=LANGUAGE,
-                         initial_k=INITIAL_K,
-                         top_k=TOP_K,
-                         include_tags=INCLUDE_TAGS)
 
 def retrieve(user_request: APIRetrievalRequest) -> Dict:
     logging.debug('Sending request to retrieval endpoint')
@@ -93,20 +58,6 @@ def retrieve(user_request: APIRetrievalRequest) -> Dict:
                              )
     logging.debug('Received response from retrieval endpoint')
     return response.json()
-
-
-class ExplainedChunk(BaseModel):
-    '''Given a `query` from a user and a retrieved `chunk` of text, provide a brief description of the content of the `chunk` and \
-whether it is relevant to the `query`.'''
-
-    # query: str = Field(..., description="An internal employee's query looking for a content such as a blog post to send to a customer.")
-    # chunk: str = Field(..., description="The retrieved text chunk from a database of chunks taken from blog posts.")
-    chain_of_thought: str = Field(..., description="Think step by step about whether the `chunk` is useful and helpful in answering the `query`.")
-    reason_why_helpful: str = Field(..., description="A brief, 1 sentence explanation on what this blog post contains and a reason whether or not it \
-is relevant based on the `query`.")
-    content_description: str = Field(..., description="A brief, 1 sentence description of the content of the blog post. \
-DO NOT say 'this blog post' or 'this chunk'. Instead, start wtih 'This covers...'.")
-    content_is_relevant: bool = Field(..., description="A boolean value indicating whether the content of the blog post is relevant to the `query`.")
 
 
 async def explain_usefulness(query, text, source, score):
@@ -128,16 +79,6 @@ async def explain_usefulness(query, text, source, score):
     return explanation, source, score
 
 
-class ExpandedQuery(BaseModel):
-    '''Given a `query` from a user, expand on what the user may be looking for in order to make \
-a semantic search match more likely.'''
-    chain_of_thought: str = Field(..., description="Think step by step about the given `query` and \
-associated machine learning and artificial intelligence topics, including industry applications, and \
-technological uses of ML and AI. Avoid concentrating excessively on any particular companies \
-or entities mentioned in the request.")
-    expanded_query: str = Field(..., description="An expanded query of machine learning topics that is more likely \
-to match a semantic search based on the users `query`.")
-
 async def expand_query(query):
     '''
     Exapnd the user query to make a semantic search match more likely
@@ -156,10 +97,6 @@ async def expand_query(query):
     
     return expanded_query
 
-
-
-class Query(BaseModel):
-    query: str
 
 app = FastAPI()
 
